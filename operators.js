d5c4f1a09d6decaeec254021644a300556026501
O-Mercy <cheria.mercy@gmail.com>
2023-10-11 15:37:32 -0500

    Completed operators exercise

---
+++ operators.js
@@ -0,0 +1,40 @@
+console.log('1' + 2);
+12
+console.log("" ** 4);
+0
+console.log(typeof '1' + 2);
+string2
+console.log(6 % 4);
+2
+(3 + 4' is ' + (3 + 4)
+
+console.log(0 < 0);
+false
+'false' == false
+false
+true == 'true'
+false
+5 >= -5
+true
+!false || false
+
+true || "42"
+
+!true && !false
+false
+6 % 5
+1
+5 < 4 && 1 === 1
+false
+typeof 'codeup' === 'string'
+true
+'codeup' === 'codeup' && 'codeup' === 'Codeup'
+false
+4 >= 0 && 1 !== '1'
+true
+6 % 3 === 0
+true
+5 % 2 !== 0
+true
+
+
