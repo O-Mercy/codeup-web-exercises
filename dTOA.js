0754884a263497ec029979ee3b61324cb20be31b
O-Mercy <cheria.mercy@gmail.com>
2023-10-11 16:57:39 -0500

    started on dTOA

---
+++ dTOA.js
@@ -0,0 +1,67 @@
+let a = 1;
+console.log(a);
+a = 1
+
+let b = a++;
+console.log(b);
+b = 1
+
+let c = ++a;
+console.log(c);
+c = 3
+// what is the value of a, b, and c?
+
+let d = "hello";
+let e = false;
+
+d++;
+e++;
+d = hello
+e = false
+let e = false;
+console.log(e);
+let d = "hello";
+console.log(d);
+
+let perplexed; // perplexed is undefined (no value is assigned)
+perplexed + 2;
+perplexed = NaN
+
+let price = 2.7;
+price.toFixed(2);
+price = 2.70
+
+let price = "2.7";
+price.toFixed(2);
+price =
+
+isNaN(0)
+
+isNaN(1)
+
+isNaN("")
+
+isNaN("string")
+
+isNaN("0")
+
+isNaN("1")
+
+isNaN("3.145")
+
+isNaN(Number.MAX_VALUE)
+
+isNaN(Infinity)
+
+isNaN("true")
+
+isNaN(true)
+
+isNaN("false")
+
+isNaN(false)
+
+// to illustrate why the isNaN() function is needed:
+NaN == NaN
+
+
