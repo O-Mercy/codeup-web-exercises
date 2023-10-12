666e793032385324a774d4559c20bac96b02015c
O-Mercy <cheria.mercy@gmail.com>
2023-10-12 00:07:33 -0500

    added comments to operators exercise

--- operators.js
+++ operators.js
@@ -1,40 +1,41 @@
 console.log('1' + 2);
-12
+// 12; concatenated 1 with 2
 console.log("" ** 4);
-0
+// 0; empty string to the power 4
 console.log(typeof '1' + 2);
-string2
+// string2; typeof string concatenated with 2
 console.log(6 % 4);
-2
-(3 + 4' is ' + (3 + 4)
-
+// 2; remainder of 6 divided by 4
+console.log('3 + 4 is ' + 3 + 4);
+console.log('3 + 4 is ' + (3+4))
+// add parenthesis to the end to make 3 + 4
 console.log(0 < 0);
-false
-'false' == false
-false
-true == 'true'
-false
-5 >= -5
-true
-!false || false
-true
-true || "42"
-true
-!true && !false
-false
-6 % 5
-1
-5 < 4 && 1 === 1
-false
-typeof 'codeup' === 'string'
-true
-'codeup' === 'codeup' && 'codeup' === 'Codeup'
-false
-4 >= 0 && 1 !== '1'
-true
-6 % 3 === 0
-true
-5 % 2 !== 0
-true
+// false; 0 is not less than 0
+console.log('false' == false);
+// false; string not equal to boolean
+console.log(true == 'true');
+// false; string not equal to boolean
+console.log(5 >= -5);
+// true; 5 is greater than or equal to -5
+console.log(!false || false);
+// true; true || false = true
+console.log(true || "42");
+// true; true || true = true
+console.log(!true && !false);
+// false; false && true = false
+console.log(6 % 5);
+// 1; remainder of 6 divided by 5
+console.log(5 < 4 && 1 === 1);
+// false; 5 is not less than 4???
+console.log(typeof 'codeup' === 'string');
+// true; type of string is equal to the string 'string'
+console.log('codeup' === 'codeup' && 'codeup' === 'Codeup');
+// false; codeup is equal to codeup but not equal to Codeup
+console.log(4 >= 0 && 1 !== '1');
+// true; 4 is greater than or equal to 0 and 1 is not equal to the type of string of 1
+console.log(6 % 3 === 0);
+// true; 2 is equal to the type and value of 0
+console.log(5 % 2 !== 0);
+// true; 1 is not equal to the value of 0
 
 
