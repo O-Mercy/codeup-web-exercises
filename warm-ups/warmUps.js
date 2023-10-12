b73a96da2820b1cd492a94e7684c5ca6b4aaf924
O-Mercy <cheria.mercy@gmail.com>
2023-10-12 09:19:39 -0500

    added space between name and 'and'

--- warm-ups/warmUps.js
+++ warm-ups/warmUps.js
@@ -2,4 +2,4 @@
 // Daily Warm-ups
 var name = 'Mercy Osugo'
 var year = '2015'
-console.log("My name is " + name + "and i graduated high school in " + year);
+console.log("My name is " + name + " and i graduated high school in " + year);
