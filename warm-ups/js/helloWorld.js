4eb44c85b9e9c13577243e2922b7d12117c45652
O-Mercy <cheria.mercy@gmail.com>
2023-10-13 09:24:12 -0500

    Exercise complete

--- warm-ups/js/helloWorld.js
+++ warm-ups/js/helloWorld.js
@@ -1,3 +1,5 @@
-let userAge = prompt('How old are you?');
+let userAge = prompt('Please enter your age?');
 console.log(`I am ${userAge}`);
-alert("Wow <userAge>, you're old!");
+// alert(`"Wow <userAge>, you're old!"`);
+// correct solution
+alert(`Wow ${userAge}, you're old!`);
