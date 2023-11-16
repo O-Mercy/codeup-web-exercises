fd76df2194f896c7a866f0c71586db66a0717375
O-Mercy <cheria.mercy@gmail.com>
2023-11-16 10:44:08 -0600

    Koname exercise, Dom-traversal lec

---
+++ js/dom-traversal.js
@@ -0,0 +1,22 @@
+const box1 = document.querySelector('.box1')
+const parent = box1.parentElement
+console.log(parent);
+
+const stateParksUL = document.getElementById("state-parks-texas");
+stateParksUL.parentElement.style.background = "lightblue";
+
+const stateParksLIs = document
+.querySelector("#state-parks-texas").children;
+for (let i = 0; i < stateParksLIs.length; i++) {
+    stateParksLIs[i].style.color = "red";
+    stateParksLIs[2].style.fontWeight = "bold";
+    stateParksLIs[4].style.visibility = "hidden"
+    stateParksLIs[1].style.fontSize = "50px"
+}
+
+const nationalParkUL = document.querySelector("#national-parks");
+nationalParkUL.firstElementChild.style.color = "green"
+nationalParkUL.lastElementChild.style.background = "pink"
+
+const nationalParksHeading = document.querySelector("#national-parks-heading");
+nationalParksHeading.nextElementSibling.style.background = "salmon"
